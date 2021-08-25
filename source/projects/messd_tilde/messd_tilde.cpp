54cc74d82b6dcfac189876be0726a9408e9390f6
maxardito <maxwellardito@gmail.com>
2021-08-25 17:21:05 -0400

    Working messdup

--- source/projects/messd_tilde/messd_tilde.cpp
+++ source/projects/messd_tilde/messd_tilde.cpp
@@ -13,20 +13,15 @@ extern "C" {
 }
 using namespace c74::min;
 
+#define MESSD_UP 1
+#define NUM_INPUTS 5
+#define NUM_OUTPUTS 4
+#define NUM_ARGS (NUM_INPUTS + NUM_OUTPUTS)
+
 class messdup : public object<messdup>, public sample_operator<1, 4> {
 private:
-    lib::sync m_oscillator;    // note: must be created prior to any attributes that might set parameters below
-    messd_t messd;
-    
-    uint16_t CLOCK_in;
-    uint16_t CLOCK_out;
-    uint16_t DOWNBEAT_in;
-    uint16_t DOWNBEAT_out;
-    uint16_t SUBDIVISION_in;
-    uint16_t SUBDIVISION_out;
-    uint16_t PHASE_in;
-    uint16_t PHASE_out;
-    bool METRIC_MODULATION_in;
+    double ins[NUM_INPUTS];
+    double outs[NUM_OUTPUTS];
 
 public:
     MIN_DESCRIPTION{ "Mess'd Up, as a Max object" };
@@ -34,24 +29,20 @@ public:
     MIN_AUTHOR{ "CuteLab" };
     MIN_RELATED{ "metro, phasor~" };
 
-    inlet<>  inlet_1{ this, "(signal) Clock (optional)" };
+    inlet<>  inlet_1{ this, "(int) Pour les messages" };
+    
     outlet<> out1{ this, "(signal) Tempo-scaled Clock", "signal" };
     outlet<> out2{ this, "(signal) Downbeats", "signal" };
     outlet<> out3{ this, "(signal) Multiplied Clock", "signal" };
     outlet<> out4{ this, "(signal) Phased Clock", "signal" };
-
-    attribute<bool> downbeat_latch{ this, "Latch to Downbeat", false };
-    attribute<bool> modulating{ this, "Modulating Active", false };
-    attribute<int, threadsafe::no, limit::clamp> scale{ this, "Interval Scale", 1, range { 1, 9 } };
-    attribute<int, threadsafe::no, limit::clamp> beats{ this, "Beats per Measure", 1, range { 1, 9 } };
-    attribute<int, threadsafe::no, limit::clamp> basis{ this, "Power of Two Basis", 0, range { -5, 5} };
-    attribute<int, threadsafe::no, limit::clamp> trunc{ this, "Measure Truncation", 0, range { 0, 8 } };
-    attribute<double, threadsafe::no, limit::wrap> phase{ this, "Phase", 0.0, range { 0.0, 1.0 } };
-    attribute<double, threadsafe::no, limit::clamp> tempo{ this, "Tempo", 120, range { 20.0, 400.0 } };
     
     messdup()
     {
         MS_init(&messd);
+        ins[CLOCK_KNOB] = 10;
+        ins[DOWNBEAT_IN] = 300;
+        ins[SUBDIVISION_IN] = 400;
+        ins[PHASE_IN] = 0;
     }
     
     ~messdup()
@@ -59,38 +50,40 @@ public:
         MS_destroy(&messd);
     }
     
-    message<> invert{ this, "invert", "Invert",
+    message<> tempo { this, "tempo", "Tempo",
         MIN_FUNCTION {
-            cout << "invert" << endl;
-            return args;
+            ins[CLOCK_KNOB] = args[0];
+            return {};
         }
     };
 
-    message<> store{ this, "store", "Store",
+    message<> beats { this, "beats", "Beats Per Measure",
         MIN_FUNCTION {
-            cout << "store" << endl;
-            return args;
+            ins[DOWNBEAT_IN] = args[0];
+            return {};
         }
     };
 
-    message<> recall{ this, "recall", "Recall",
+    message<> subdivision { this, "subdivision", "Subdivision Per Measure",
         MIN_FUNCTION {
-            cout << "recall" << endl;
-            return args;
+            ins[SUBDIVISION_IN] = args[0];
+            return {};
         }
     };
 
-    message<> reset{ this, "reset", "Reset",
+    message<> phase { this, "phase", "Phased rhythm",
         MIN_FUNCTION {
-            cout << "reset" << endl;
-            return args;
+            ins[PHASE_IN] = args[0];
+            return {};
         }
     };
 
-    samples<4> operator()(sample in1) {
-        m_oscillator.frequency(tempo / 60.0, samplerate());
-        double op = m_oscillator();
-        return { op, op, op, op };
+
+    samples<4> operator()(sample in){
+        
+        MS_process(&messd, ins, outs);
+            
+        return { outs[0], outs[1], outs[2], outs[3] };
     }
 };
 
